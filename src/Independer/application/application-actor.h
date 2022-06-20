3dd77ada3ab2e6d33c30c8edbdeb196fbe0963ee
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2022-06-20 14:55:41 +0200

    implement who is in my area

--- src/Independer/application/application-actor.h
+++ src/Independer/application/application-actor.h
@@ -15,7 +15,7 @@ void application_actor_who_is_in_my_area() {
     l_attempt++;
 
     lora_send_msg_short_message(state_my_id, "*", "?", state_lora_gain);
-    // delay(C_INDEPENDER_SEND_DELAY);
+    delay(int(C_INDEPENDER_SEND_DELAY/2));
 
     gui_display_prg_static("Versuch", l_attempt, 0, c_max_ping_retries);
 
