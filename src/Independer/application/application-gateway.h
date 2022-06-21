9706538acd55e987f54bff36768be0336eeb4e1b
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2022-06-21 17:02:32 +0200

    implement messages

--- src/Independer/application/application-gateway.h
+++ src/Independer/application/application-gateway.h
@@ -5,8 +5,31 @@
  */
 
 void application_gateway_store_msg(String from, String messageString) {
-    Serial.println("Should store message '" + messageString + "' from '" + from + "'");
-    //TODO
+
+  messageString = messageString.substring(2, messageString.length());
+
+  String p_receiver_id = "";
+  String p_message = "";
+
+  int currentCountsDelimiter = 0;
+
+  for (int i = 0; i < messageString.length(); i++) {
+
+    String current = messageString.substring(i, i + 1);
+
+    if (current == ";") {
+      currentCountsDelimiter++;
+    } else {
+      if (currentCountsDelimiter == 0) p_receiver_id += current;
+      if (currentCountsDelimiter == 1) p_message += current;
+    }
+  }
+
+  Serial.println("Extracted p_receiver_id '" + p_receiver_id + "'");
+  Serial.println("Extracted p_message '" + p_message + "'");
+
+  //TODO
+
 }
 
 /*
