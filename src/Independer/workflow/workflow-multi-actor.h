42f7de2d4b37376118ac662a48f14b77f3cffac9
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2022-06-24 16:15:08 +0200

    implement msg between actors

--- src/Independer/workflow/workflow-multi-actor.h
+++ src/Independer/workflow/workflow-multi-actor.h
@@ -9,6 +9,9 @@ TaskHandle_t Core1TaskHnd;
 
 String state_ticker_multi_actor_rec_actor_id = "";
 
+String state_multi_actor_msgs[100] = "";
+int state_multi_actor_msgs_counter = 0;
+
 void i_multi_res_proc_actor_rec_message_from_actor(void *parameter)
 {
   String actorId = state_ticker_multi_actor_rec_actor_id;
@@ -52,8 +55,9 @@ void i_multi_res_proc_actor_rec_message_from_actor(void *parameter)
         {
           delay(C_INDEPENDER_SEND_DELAY);
           lora_send_msg_single_unsafe(state_my_id, actorId, "N", state_lora_gain);
-          Serial.println("Direct msg from " + actorId + ": '" + pong_ans.message + "'");
-          // TODO
+          Serial.println("Save Direct msg from " + actorId + ": '" + pong_ans.message + "'");
+          state_multi_actor_msgs[state_multi_actor_msgs_counter] = "(" + actorId + ") " + pong_ans.message;
+          state_multi_actor_msgs_counter++;
           sendSuccess = true;
         }
       }
@@ -83,67 +87,6 @@ void i_multi_actor_rec_message_from_actor(String actorId)
 
 boolean state_multi_is_active = false;
 
-// const int MULTI_STRING_BUFFER_MAX = 50;
-
-// int state_multi_cur_string_i = 0;
-// String state_multi_sting_buffer[MULTI_STRING_BUFFER_MAX];
-
-// Maybe deprecated, important for string buffer
-//  void i_multi_Task1_string_buffer(void * parameter) {
-//    for (;;) {
-//      // Serial.print("multi_Task1 runs on Core: ");
-//      // Serial.println(xPortGetCoreID());
-
-//     int packetSize = LoRa.parsePacket();
-
-//     if (packetSize) {
-
-//     //   Serial.println("Got packet");
-//       String i_res = "";
-//       for (int i = 0; i < packetSize; i++) {
-//         i_res += (char) LoRa.read();
-//       }
-
-//     //   Serial.println("Before parse '" + i_res + "'");
-
-//       if (state_multi_cur_string_i < MULTI_STRING_BUFFER_MAX) {
-//         state_multi_sting_buffer[state_multi_cur_string_i] = i_res;
-//         state_multi_cur_string_i++;
-
-//         //Try to parse in this loop
-//         ParserAnsTuple parser_ans = lora_stateful_parse(i_res, state_gateway_id);
-//         if (parser_ans.message != "") {
-//         Serial.println("Got ac-msg '" + String(parser_ans.message) + "'");
-//         String msg = String(LoRa.packetRssi(), DEC) + " " + String(utils_get_battery()) + " oD";
-//         lora_send_msg_single_unsafe(state_my_id, parser_ans.from, msg, state_lora_gain);
-//         }
-
-//       } else {
-//         Serial.println("Error Buffer OverFlow in Message");
-//       }
-
-//     }
-
-//     // delay (700); //Default
-//     delay(10); //Between Trans
-//   }
-// }
-
-// void i_multi_actor_proc_string_buffer() {
-
-//   for (int i = 0; i < state_multi_cur_string_i; i++) {
-//     Serial.println("\nGot i " + String(i));
-
-//     ParserAnsTuple parser_ans = lora_stateful_parse(state_multi_sting_buffer[i], state_gateway_id);
-
-//     if (parser_ans.message != "") {
-//       Serial.println("Got message inactive'" + String(parser_ans.message) + "'");
-//     }
-//   }
-
-//   state_multi_cur_string_i = 0;
-// }
-
 void i_multi_Task1_short_message(void *parameter)
 {
   for (;;)
@@ -151,8 +94,6 @@ void i_multi_Task1_short_message(void *parameter)
 
     if (state_ticker_multi_actor_rec_actor_id == "")
     {
-      // Serial.print("multi_Task1 runs on Core: ");
-      // Serial.println(xPortGetCoreID());
 
       int packetSize = LoRa.parsePacket();
 
@@ -200,7 +141,6 @@ void i_multi_Task1_short_message(void *parameter)
 void multi_actor_start()
 {
   state_multi_is_active = true;
-  // state_multi_cur_string_i = 0;
   xTaskCreatePinnedToCore(i_multi_Task1_short_message, "CPU_1m", 1000 * 2, NULL, 1, &Core1TaskHnd, 1); // TODO: Warum Faktor 2
 }
 
@@ -218,11 +158,7 @@ boolean multi_actor_get_state()
 String multi_actor_get_state_string()
 {
 
-  // String post_string = " Buffer " + String(state_multi_cur_string_i);
-
-  //   i_multi_actor_proc_string_buffer();
-
   if (state_multi_is_active)
-    return "Aktiv"; // + post_string;
+    return "Aktiv";
   return "Inaktiv";
 }
