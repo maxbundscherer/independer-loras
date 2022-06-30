dbef8a3a8fcf8a79a3fcd80d4226c28a99e880fe
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2022-06-30 15:44:32 +0200

    implement db

--- src/Independer/Independer.ino
+++ src/Independer/Independer.ino
@@ -29,7 +29,7 @@
  * ####################################
  */
 // Product Config
-String c_product_version = "v.0.1.4";
+String c_product_version = "v.0.1.5";
 boolean c_dev_mode = true;
 boolean c_actor_mode = true;
 
