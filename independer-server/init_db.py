9b4f0a130e51506d1a9a522779d5a71a36f4832c
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2022-07-24 20:29:29 +0200

    Add Flask DB Init

--- independer-server/init_db.py
+++ independer-server/init_db.py
@@ -2,6 +2,8 @@ import config as conf
 
 import psycopg2
 
+print("Started Init DB")
+
 conn = psycopg2.connect(
     host=conf.getDbConfigHost(),
     database=conf.getDbConfigDatabase(),
@@ -14,34 +16,43 @@ cur = conn.cursor()
 
 # Execute a command: this creates a new table
 cur.execute('DROP TABLE IF EXISTS books;')
-cur.execute('CREATE TABLE books (id serial PRIMARY KEY,'
-            'title varchar (150) NOT NULL,'
-            'author varchar (50) NOT NULL,'
-            'pages_num integer NOT NULL,'
-            'review text,'
+cur.execute('CREATE TABLE messages (id serial PRIMARY KEY,'
+            'receiver varchar (5) NOT NULL,'
+            'author varchar (5) NOT NULL,'
+            'msg text,'
+            'active boolean,'
             'date_added date DEFAULT CURRENT_TIMESTAMP);'
             )
 
 # Insert data into the table
 
-cur.execute('INSERT INTO books (title, author, pages_num, review)'
+cur.execute('INSERT INTO messages (receiver, author, msg, active)'
             'VALUES (%s, %s, %s, %s)',
-            ('A Tale of Two Cities',
-             'Charles Dickens',
-             489,
-             'A great classic!')
+            ('0xM2',
+             '0xMB',
+             'Hello world!',
+             True)
             )
 
-
-cur.execute('INSERT INTO books (title, author, pages_num, review)'
+cur.execute('INSERT INTO messages (receiver, author, msg, active)'
             'VALUES (%s, %s, %s, %s)',
-            ('Anna Karenina',
-             'Leo Tolstoy',
-             864,
-             'Another great classic!')
+            ('0xM2',
+             '0xMB',
+             'Hello world 2!',
+             False)
+            )
+
+cur.execute('INSERT INTO messages (receiver, author, msg, active)'
+            'VALUES (%s, %s, %s, %s)',
+            ('0xMB',
+             '0xM2',
+             'Hello world 3!',
+             True)
             )
 
 conn.commit()
 
 cur.close()
 conn.close()
+
+print("Finished Init DB")
