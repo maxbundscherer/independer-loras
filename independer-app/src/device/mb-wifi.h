0355da183c7963ea40e6fff7211a2bb87fd91d3a
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2022-08-04 14:59:40 +0200

    Implement Delete Messages

--- independer-app/src/device/mb-wifi.h
+++ independer-app/src/device/mb-wifi.h
@@ -223,3 +223,87 @@ String wifi_get_chat_messages(String myId, String serverUrl, int serverPort, int
 
     return ret_status;
 }
+
+/*
+ * ####################################
+ *  Clear Chat Message Section
+ * ####################################
+ */
+
+boolean wifi_clear_message(String myId, String serverUrl, int serverPort, int serverTimeout)
+{
+
+    char *c_wifi_server_url = const_cast<char *>(serverUrl.c_str());
+    int c_wifi_server_port = serverPort;
+    int c_wifi_timeout = serverTimeout;
+
+    boolean ret_status = false;
+
+    if (i_wifi_connect())
+    {
+
+        // Code Start
+        Serial.println("[Send GET REQUEST] START");
+        Serial.println("Connect to server (timeout " + String(c_wifi_timeout) + ")");
+        if (!i_wifi_client.connect(c_wifi_server_url, c_wifi_server_port, c_wifi_timeout))
+        {
+            Serial.println("Connection failed!");
+        }
+        else
+        {
+            Serial.println("Connected to server!");
+            /* create HTTP request */
+
+            int c_mail_max_fails = 150;
+
+            int mail_fail_connect_counter = 0;
+
+            String send_string = String("GET ") + "/v1/clearmsg/" + myId + " HTTP/1.1\r\n" +
+                                 "Host: " + String(c_wifi_server_url) + ":" + String(c_wifi_server_port) + "\r\n" +
+                                 "Content-Type: application/json" + "\r\n" +
+                                 "Connection: close\r\n\r\n";
+
+            Serial.println("Send String is '" + send_string + "'");
+
+            i_wifi_client.print(send_string);
+
+            Serial.println("[Waiting for response] START");
+            while (!i_wifi_client.available() and mail_fail_connect_counter < c_mail_max_fails)
+            {
+                mail_fail_connect_counter++;
+                Serial.print("AM=" + String(mail_fail_connect_counter) + "/" + String(c_mail_max_fails) + " ");
+                delay(50); //
+            }
+            Serial.println("\n[Waiting for response] STOP");
+
+            String line = "";
+            while (i_wifi_client.available())
+            {
+                line += i_wifi_client.readStringUntil('\r');
+            }
+            Serial.print("Response '" + line + "'");
+
+            if (line.endsWith("OK"))
+            {
+                ret_status = true;
+            }
+
+            /* if the server disconnected, stop the client */
+            if (!i_wifi_client.connected())
+            {
+                Serial.println();
+                Serial.println("Server disconnected");
+            }
+
+            i_wifi_client.stop();
+        }
+        Serial.println("[Send GET Request] STOP");
+        // Mail Cod Stop
+    }
+    else
+        Serial.println("Could not send mail: WiFi is not connected");
+
+    i_wifi_disconnect();
+
+    return ret_status;
+}
