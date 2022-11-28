cb25f407a7fa6b0b96333ebae349f95b3e31b674
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2022-11-28 14:17:47 +0100

    Add german trans

--- independer-app/src/device/i18n/german.h
+++ independer-app/src/device/i18n/german.h
@@ -3,30 +3,30 @@ void i18n_display_lang()
     Serial.println("Set to language: German");
 }
 
-String I18N_ERROR_TITLE = "Fehler";
+const String I18N_ERROR_TITLE = "Fehler";
 
-String I18N_ACTOR_MAIN_MENU_TITLE = "Independer";
-String I18N_ACTOR_MAIN_MENU_COMMUNICATIONS = "Kommunikation";
-String I18N_ACTOR_MAIN_MENU_ACTOR_FUNCTIONS = "Actor Funktionen";
-String I18N_ACTOR_MAIN_MENU_GATEWAY_FUNCTIONS = "Gateway Funktionen";
-String I18N_ACTOR_MAIN_MENU_SETTINGS = "Einstellungen";
-String I18N_ACTOR_MAIN_MENU_INFO = "Info";
-String I18N_ACTOR_MAIN_MENU_ERR = "Auswahl nicht verfügbar.";
-String I18N_ACTOR_MAIN_REWRITE_BOOL_ACTIV = "Aktiv";
-String I18N_ACTOR_MAIN_REWRITE_BOOL_INACTIV = "Inaktiv";
+const String I18N_ACTOR_MAIN_MENU_TITLE = "Independer";
+const String I18N_ACTOR_MAIN_MENU_COMMUNICATIONS = "Kommunikation";
+const String I18N_ACTOR_MAIN_MENU_ACTOR_FUNCTIONS = "Actor Funktionen";
+const String I18N_ACTOR_MAIN_MENU_GATEWAY_FUNCTIONS = "Gateway Funktionen";
+const String I18N_ACTOR_MAIN_MENU_SETTINGS = "Einstellungen";
+const String I18N_ACTOR_MAIN_MENU_INFO = "Info";
+const String I18N_ACTOR_MAIN_MENU_ERR = "Auswahl nicht verfügbar.";
+const String I18N_ACTOR_MAIN_REWRITE_BOOL_ACTIV = "Aktiv";
+const String I18N_ACTOR_MAIN_REWRITE_BOOL_INACTIV = "Inaktiv";
 
-String I18N_ACTOR_MAIN_MENU_INFO_PREFIX = "Info";
-String I18N_ACTOR_MAIN_MENU_INFO_VERSION = "Version: ";
-String I18N_ACTOR_MAIN_MENU_INFO_ACTOR_MODE = "Aktor-Modus: ";
-String I18N_ACTOR_MAIN_MENU_INFO_BRIGHTNESS = "Helligkeit: ";
-String I18N_ACTOR_MAIN_MENU_INFO_BACKGROUND_SYNC = "Hintergrundsync: ";
-String I18N_ACTOR_MAIN_MENU_INFO_TIME = "Zeit:\n";
-String I18N_ACTOR_MAIN_MENU_INFO_ID = "\nID: ";
-String I18N_ACTOR_MAIN_MENU_INFO_GID = "\nGateway ID: ";
-String I18N_ACTOR_MAIN_MENU_INFO_DEVELOPER = "\nEntwickler: ";
-String I18N_ACTOR_MAIN_MENU_INFO_LORA_GAIN = "\nLoRa-Gain: ";
-String I18N_ACTOR_MAIN_MENU_INFO_WIFI = "\nWIFI: ";
-String I18N_ACTOR_MAIN_MENU_INFO_WIFI_T = "\nWIFI-Timeout: ";
-String I18N_ACTOR_MAIN_MENU_INFO_AUTO_SLP = "\nAuto-Schlaf: ";
-String I18N_ACTOR_MAIN_MENU_INFO_BATTERY = "\nBatterie: ";
-String I18N_ACTOR_MAIN_MENU_INFO_BATTERY_U = "mV";
+const String I18N_ACTOR_MAIN_MENU_INFO_PREFIX = "Info";
+const String I18N_ACTOR_MAIN_MENU_INFO_VERSION = "Version: ";
+const String I18N_ACTOR_MAIN_MENU_INFO_ACTOR_MODE = "Aktor-Modus: ";
+const String I18N_ACTOR_MAIN_MENU_INFO_BRIGHTNESS = "Helligkeit: ";
+const String I18N_ACTOR_MAIN_MENU_INFO_BACKGROUND_SYNC = "Hintergrundsync: ";
+const String I18N_ACTOR_MAIN_MENU_INFO_TIME = "Zeit:\n";
+const String I18N_ACTOR_MAIN_MENU_INFO_ID = "\nID: ";
+const String I18N_ACTOR_MAIN_MENU_INFO_GID = "\nGateway ID: ";
+const String I18N_ACTOR_MAIN_MENU_INFO_DEVELOPER = "\nEntwickler: ";
+const String I18N_ACTOR_MAIN_MENU_INFO_LORA_GAIN = "\nLoRa-Gain: ";
+const String I18N_ACTOR_MAIN_MENU_INFO_WIFI = "\nWIFI: ";
+const String I18N_ACTOR_MAIN_MENU_INFO_WIFI_T = "\nWIFI-Timeout: ";
+const String I18N_ACTOR_MAIN_MENU_INFO_AUTO_SLP = "\nAuto-Schlaf: ";
+const String I18N_ACTOR_MAIN_MENU_INFO_BATTERY = "\nBatterie: ";
+const String I18N_ACTOR_MAIN_MENU_INFO_BATTERY_U = "mV";
